ce2a26dad1bbb9a950d853017ee43321bec3279d
Yogitha <yogitha@gmail.com>
2026-01-15 10:06:19 +0530

    Fix ProblemIdentifier and ProblemData definitions, pass flake8 and mypy

--- src/domain/models/problem.py
+++ src/domain/models/problem.py
@@ -3,19 +3,16 @@ from typing import List, Optional
 
 
 @dataclass
+class ProblemIdentifier:
+    contest_id: str
+    problem_id: str
+    is_gym: bool = False
+
+
+@dataclass
 class ProblemData:
     identifier: ProblemIdentifier
     title: str
     url: str
     contest_name: Optional[str]
     possible_editorial_links: List[str]
-
-
-
-from dataclasses import dataclass
-
-@dataclass
-class ProblemIdentifier:
-    contest_id: str
-    problem_id: str
-    is_gym: bool = False
